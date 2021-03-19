9615bf2b552f5f9631e674dbece62a55a1f77e81
Sneha Abraham <sneha.abraham@ftsindia.in>
2021-03-19 16:58:31 +0530

    responsive-fix

--- src/views/TryB4All.vue
+++ src/views/TryB4All.vue
@@ -151,10 +151,10 @@
               </p>
             </div>
           </b-col>
-          <b-col cols="1" xl="6">
+          <b-col cols="12" xl="6">
             <div class="biometric-img-block">
               <b-row
-                class="justify-content-center align-items-center gutter-20 flex-column"
+                class="justify-content-center align-items-center gutter-20 flex-md-column"
               >
                 <b-col cols="12" md="7">
                   <div class="bg-biometric bg-black">
